d0e1e12e2b8dea120017367ce3abc138baff946d
Susan Tan <ssusantan9@gmail.com>
2019-04-07 18:36:02 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,15 @@
+require 'pry'
+
 class Person
-  #your code here
+
+def initialization()
+  @name = name
+  @hair_color = hair_color
+  @eye_color = eye_color
+  @height = height
+  @weight = weight
+  @handed = handed
+  @complextion = complextion
+  @t_shirt_size = t_shirt_size
+
 end
