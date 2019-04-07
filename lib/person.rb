d4539e05009ad54a263699369b949694687e4b7a
Susan Tan <ssusantan9@gmail.com>
2019-04-07 19:31:50 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,33 +1,21 @@
-# require 'pry'
-
-# class Person
-  
-# attr_accessor :name, :birthday, :hair_color, :eye_color, :height,
-# :weight, :handed, :complexion, :t_shirt_size, 
-# :wrist_size, :glove_size, :pant_length, :pant_width
-
-# def initialize(attr_accessor)
-#   attr_accessor.each do |k, v|
-#     self.send(("#{k}="), value)
-#   end 
-# end 
-
-# end
-
-
-# # a = Person.new()
-# # params.each do |key, value|
-# #   a.send("#{key}=", value)
-# # end
+require 'pry'
 
 class Person
-  attr_accessor :name, :birthday, :hair_color, :eye_color, :height,
-                :weight, :handed, :complexion, :t_shirt_size,
-                :wrist_size, :glove_size, :pant_length, :pant_width
+  
+attr_accessor :name, :birthday, :hair_color, :eye_color, :height,
+:weight, :handed, :complexion, :t_shirt_size, 
+:wrist_size, :glove_size, :pant_length, :pant_width
 
-  def initialize(attributes)
-    attributes.each do |key, value|
-      self.send(("#{key}="), value)
-    end
-  end
+def initialize(attrs)
+  attrs.each do |k, v|
+    self.send("#{k}=", value)
+  end 
+end 
+
 end
+
+
+# a = Person.new()
+# params.each do |key, value|
+#   a.send("#{key}=", value)
+# end
