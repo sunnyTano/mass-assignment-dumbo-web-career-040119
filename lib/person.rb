c5fc97cc227e30cde2ba7e78fddb33a7b127750c
Susan Tan <ssusantan9@gmail.com>
2019-04-07 18:36:09 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -2,7 +2,9 @@ require 'pry'
 
 class Person
 
-def initialization()
+def initialization(:name, :birthday, :hair_color, :eye_color,:height, 
+:weight, :handed, :complexion, :t_shirt_size, 
+:wrist_size, :glove_size, :pant_length, :pant_width)
   @name = name
   @hair_color = hair_color
   @eye_color = eye_color
