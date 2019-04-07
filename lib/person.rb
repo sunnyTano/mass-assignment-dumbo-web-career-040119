baeea44daf0c00cf120af3b25c832e6a0bd34c52
Susan Tan <ssusantan9@gmail.com>
2019-04-07 19:11:08 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -9,7 +9,7 @@ attr_accessor :name, :birthday, :hair_color, :eye_color, :height,
 def initialize(attr)
   count = ""
   attr.map do |k, v|
-    self.send("#{k} = #{v}")
+    self.send(k, ("= #{v}"))
   end 
 end 
 
