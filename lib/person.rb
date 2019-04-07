bc342aa06271c7f5773dbfa38ca75941870d8388
Susan Tan <ssusantan9@gmail.com>
2019-04-07 18:41:45 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -6,17 +6,7 @@ class Person
 :weight, :handed, :complexion, :t_shirt_size, 
 :wrist_size, :glove_size, :pant_length, :pant_width
 
-def initialization(:name, :birthday, :hair_color, :eye_color,:height, 
-:weight, :handed, :complexion, :t_shirt_size, 
-:wrist_size, :glove_size, :pant_length, :pant_width)
-  @name = name
-  @hair_color = hair_color
-  @eye_color = eye_color
-  @height = height
-  @weight = weight
-  @handed = handed
-  @complextion = complextion
-  @t_shirt_size = t_shirt_size
+def initialization()
 end 
 
 end
