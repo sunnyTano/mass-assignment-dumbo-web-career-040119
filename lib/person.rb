0b2022db975b37e30c4cd2f1240481026d217136
Susan Tan <ssusantan9@gmail.com>
2019-04-07 19:30:46 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -19,3 +19,15 @@
 # # params.each do |key, value|
 # #   a.send("#{key}=", value)
 # # end
+
+class Person
+  attr_accessor :name, :birthday, :hair_color, :eye_color, :height,
+                :weight, :handed, :complexion, :t_shirt_size,
+                :wrist_size, :glove_size, :pant_length, :pant_width
+
+  def initialize(attributes)
+    attributes.each do |key, value|
+      self.send(("#{key}="), value)
+    end
+  end
+end
