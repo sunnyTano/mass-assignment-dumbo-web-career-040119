36b0a1e2008253987a97e226a0777c4514f3b28e
Susan Tan <ssusantan9@gmail.com>
2019-04-07 19:40:44 +0000

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,24 @@
+require 'pry'
+
 class Person
-  #your code here
+  
+attr_accessor :name, :birthday, :hair_color, :eye_color, :height,
+:weight, :handed, :complexion, :t_shirt_size, 
+:wrist_size, :glove_size, :pant_length, :pant_width
+
+def initialize(attrs)
+  attrs.each do |k, v|
+    self.send("#{k}=", v) # .send => dynamically call on method. 
+    #puts k, v
+  end 
+end 
+
 end
+
+
+#    Example 
+# a = Person.new()
+# params.each do |key, value|
+#   a.send("#{key}=", value)
+# end
+
