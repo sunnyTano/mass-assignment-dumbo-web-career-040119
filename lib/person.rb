525d58f9eda47c30f4a0ea92752508abdb308a0c
Susan Tan <ssusantan9@gmail.com>
2019-04-07 18:45:15 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -6,7 +6,7 @@ attr_accessor :name, :birthday, :hair_color, :eye_color, :height,
 :weight, :handed, :complexion, :t_shirt_size, 
 :wrist_size, :glove_size, :pant_length, :pant_width
 
-def initialization(attr)
+def initialize(attr)
   
   attr.map do |k, v|
     puts "#{k} = #{v}"
