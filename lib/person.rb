2a99c711427bc2ecb755034922c99b73fba9952f
Susan Tan <ssusantan9@gmail.com>
2019-04-07 18:41:58 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -2,11 +2,11 @@ require 'pry'
 
 class Person
   
-  attr_accessor :name, :birthday, :hair_color, :eye_color,:height, 
-:weight, :handed, :complexion, :t_shirt_size, 
-:wrist_size, :glove_size, :pant_length, :pant_width
+  attr_accessor 
 
 def initialization()
 end 
 
 end
+
+
